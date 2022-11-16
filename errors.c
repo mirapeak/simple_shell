14335d65221e01ab1ae32fc50cc72716d8355860
mirapeak <livinusmiracle2009@yahoo.com>
2022-11-15 18:11:56 -0800

    Paired Project - Simple Shell

--- errors.c
+++ errors.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-​
+
 /**
  *_eputs - prints an input string
  * @str: the string to be printed
@@ -8,17 +8,17 @@
  */
 void _eputs(char *str)
 {
-  int i = 0;
-​
-  if (!str)
-    return;
-  while (str[i] != '\0')
-    {
-      _eputchar(str[i]);
-      i++;
-    }
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
 }
-​
+
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
@@ -28,19 +28,19 @@ void _eputs(char *str)
  */
 int _eputchar(char c)
 {
-  static int i;
-  static char buf[WRITE_BUF_SIZE];
-​
-  if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-      write(2, buf, i);
-      i = 0;
-    }
-  if (c != BUF_FLUSH)
-    buf[i++] = c;
-  return (1);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
-​
+
 /**
  * _putfd - writes the character c to given fd
  * @c: The character to print
@@ -51,19 +51,19 @@ int _eputchar(char c)
  */
 int _putfd(char c, int fd)
 {
-  static int i;
-  static char buf[WRITE_BUF_SIZE];
-​
-  if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-      write(fd, buf, i);
-      i = 0;
-    }
-  if (c != BUF_FLUSH)
-    buf[i++] = c;
-  return (1);
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
-​
+
 /**
  *_putsfd - prints an input string
  * @str: the string to be printed
@@ -73,13 +73,13 @@ int _putfd(char c, int fd)
  */
 int _putsfd(char *str, int fd)
 {
-  int i = 0;
-​
-  if (!str)
-    return (0);
-  while (*str)
-    {
-      i += _putfd(*str++, fd);
-    }
-  return (i);
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
--- errors1.c
+++ errors1.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-#include "shell.h"
 
 /**
  * _erratoi - converts a string to an integer
@@ -9,27 +8,26 @@
  */
 int _erratoi(char *s)
 {
-  int i = 0;
+	int i = 0;
+	unsigned long int result = 0;
 
-unsigned long int result = 0;
-
-  if (*s == '+')
-    s++;  /* TODO: why does this make main return 255? */
-  for (i = 0;  s[i] != '\0'; i++)
-    {
-      if (s[i] >= '0' && s[i] <= '9')
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
 	{
-	  result *= 10;
-	  result += (s[i] - '0');
-	  if (result > INT_MAX)
-	    return (-1);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
 	}
-      else
-	return (-1);
-    }
-  return (result);
+	return (result);
 }
-​
+
 /**
  * print_error - prints an error message
  * @info: the parameter & return info struct
@@ -39,15 +37,15 @@ unsigned long int result = 0;
  */
 void print_error(info_t *info, char *estr)
 {
-  _eputs(info->fname);
-  _eputs(": ");
-  print_d(info->line_count, STDERR_FILENO);
-  _eputs(": ");
-  _eputs(info->argv[0]);
-  _eputs(": ");
-  _eputs(estr);
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
 }
-​
+
 /**
  * print_d - function prints a decimal (integer) number (base 10)
  * @input: the input
@@ -57,36 +55,36 @@ void print_error(info_t *info, char *estr)
  */
 int print_d(int input, int fd)
 {
-  int (*__putchar)(char) = _putchar;
-  int i, count = 0;
-  unsigned int _abs_, current;
-​
-  if (fd == STDERR_FILENO)
-    __putchar = _eputchar;
-  if (input < 0)
-    {
-      _abs_ = -input;
-      __putchar('-');
-      count++;
-    }
-  else
-    _abs_ = input;
-  current = _abs_;
-  for (i = 1000000000; i > 1; i /= 10)
-    {
-      if (_abs_ / i)
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
 	{
-	  __putchar('0' + current / i);
-	  count++;
+		_abs_ = -input;
+		__putchar('-');
+		count++;
 	}
-      current %= i;
-    }
-  __putchar('0' + current);
-  count++;
-​
-  return (count);
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
 }
-​
+
 /**
  * convert_number - converter function, a clone of itoa
  * @num: number
@@ -97,32 +95,32 @@ int print_d(int input, int fd)
  */
 char *convert_number(long int num, int base, int flags)
 {
-  static char *array;
-  static char buffer[50];
-  char sign = 0;
-  char *ptr;
-  unsigned long n = num;
-​
-  if (!(flags & CONVERT_UNSIGNED) && num < 0)
-    {
-      n = -num;
-      sign = '-';
-​
-    }
-  array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-  ptr = &buffer[49];
-  *ptr = '\0';
-​
-  do{
-    *--ptr = array[n % base];
-    n /= base;
-  } while (n != 0);
-​
-  if (sign)
-    *--ptr = sign;
-  return (ptr);
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
 }
-​
+
 /**
  * remove_comments - function replaces first instance of '#' with '\0'
  * @buf: address of the string to modify
@@ -131,12 +129,12 @@ char *convert_number(long int num, int base, int flags)
  */
 void remove_comments(char *buf)
 {
-  int i;
-​
-  for (i = 0; buf[i] != '\0'; i++)
-    if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
-      {
-	buf[i] = '\0';
-	break;
-      }
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
 }
